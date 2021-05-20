0d85eaeb0575ed5e908caf7b6f9afa2167a05b42
kiptok <kiptok@gmail.com>
2021-05-20 00:38:00 -0400

    switched to fmod

--- Assets/Scripts/Audio.cs
+++ Assets/Scripts/Audio.cs
@@ -5,43 +5,66 @@ using UnityEngine;
 public class Audio : MonoBehaviour {
 
     public SquidController squid;
-    private AudioSource music;
-    private AudioLowPassFilter lpf;
 
-    public float minCutoff;
-    public float maxCutoff;
+    //EventRef presents the designer with UI for selecting events
+    [FMODUnity.EventRef]
+    public string GameMusicEvent = "";
 
-    private bool filtering;
+    //EventInstance allows us to manage an event over its lifetime
+    FMOD.Studio.EventInstance gameMusic;
+
+    private int squidY;
+    FMOD.Studio.PARAMETER_ID squidYParameterId;
+
+    private bool _inWater;
+
+    private Transform squidTransform;
+    private Rigidbody2D squidRigidBody;
 
     private void Awake() {
-        music = GetComponent<AudioSource>();
-        lpf = GetComponent<AudioLowPassFilter>();
         SquidController.OnDive += OnSquidDive;
         SquidController.OnSurface += OnSquidSurface;
     }
 
+    void Start() {
+        squidRigidBody = squid.GetComponent<Rigidbody2D>();
+        squidTransform = squid.GetComponent<Transform>();
+
+        _inWater = true;
+
+        //Create an instance of an Event and manually start it
+        gameMusic = FMODUnity.RuntimeManager.CreateInstance(GameMusicEvent);
+        gameMusic.start();
+
+        //FMODUnity.RuntimeManager.AttachInstanceToGameObject(gameMusic, squid.GetComponent<Transform>(), squid.GetComponent<Rigidbody2D>())
+
+        // Cache a handle to the "squidY" parameter for usage in Update()
+        FMOD.Studio.EventDescription squidYEventDescription;
+        gameMusic.getDescription(out squidYEventDescription);
+        FMOD.Studio.PARAMETER_DESCRIPTION squidYParameterDescription;
+        squidYEventDescription.getParameterDescriptionByName("SquidY", out squidYParameterDescription);
+        squidYParameterId = squidYParameterDescription.id;
+    }
+
     private void OnDestroy() {
+        gameMusic.release();
         SquidController.OnDive -= OnSquidDive;
         SquidController.OnSurface -= OnSquidSurface;
     }
 
-    void Start() {
-        filtering = true;
-    }
-
     // Update is called once per frame
     void Update() {
-        if (filtering) {
-//            lpf.cutoffFrequency = maxCutoff * (minCutoff / maxCutoff) * Mathf.Pow()
-            lpf.cutoffFrequency = Mathf.Lerp(maxCutoff, minCutoff, squid.transform.position.y / -100f);
+        if (_inWater) {
+            gameMusic.setParameterByID(squidYParameterId, squid.transform.position.y);
+            Debug.Log(squid.transform.position.y);
         }
     }
 
     private void OnSquidDive(SquidController sq) {
-        filtering = true;
+        _inWater = true;
     }
 
     private void OnSquidSurface(SquidController sq) {
-        filtering = false;
+        _inWater = false;
     }
 }
